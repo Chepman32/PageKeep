d6c4a831e99724675aa2620cdaa38f84f560d916
Chepman32 <antonkerch555@gmail.com>
2025-11-02 11:43:18 +0300

    App

--- App.tsx
+++ App.tsx
@@ -4,14 +4,10 @@ import { FileSystem } from './src/utils/fileSystem';
 import { getDatabase } from './src/data/database';
 import { ThemeProvider } from './src/contexts/ThemeContext';
 import './src/locales';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { IncomingShareService } from './src/services/IncomingShareService';
 
 const App: React.FC = () => {
   useEffect(() => {
-    MaterialCommunityIcons.loadFont().catch(error =>
-      console.warn('Failed to load icon font:', error),
-    );
     initializeApp();
   }, []);
 
--- index.js
+++ index.js
@@ -4,7 +4,12 @@
 
 import 'react-native-get-random-values';
 import { AppRegistry } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import App from './App';
 import { name as appName } from './app.json';
+
+MaterialCommunityIcons.loadFont().catch(error => {
+  console.warn('Failed to load MaterialCommunityIcons font:', error);
+});
 
 AppRegistry.registerComponent(appName, () => App);
